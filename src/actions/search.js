5a1b883bccc6ed0ab9b8ba0ecc73ccd49f22e754
brijesh panchal <brijeshpanchal127@gmail.com>
2021-04-04 21:23:09 -0400

    moved constants to env

--- src/actions/search.js
+++ src/actions/search.js
@@ -1,14 +1,13 @@
-import axios from "axios"
-import { API_HOST, API_KEY, SEARCH_URL } from "../api/url.constant";
-import { SET_RESULTS } from "../constants/action.types"
+import axios from "axios";
+import { SET_RESULTS } from "../constants/action.types";
 
 export const searchVideos = async (searchText) => {
     const options = {
         method: 'GET',
-        url: SEARCH_URL + searchText,
+        url: process.env.REACT_APP_SEARCH_URL + searchText,
         headers: {
-            'x-rapidapi-key': API_KEY,
-            'x-rapidapi-host': API_HOST
+            'x-rapidapi-key': process.env.REACT_APP_API_KEY,
+            'x-rapidapi-host': process.env.REACT_APP_API_HOST
         }
     };
     const response = await axios.request(options);
--- src/api/url.constant.js
+++
@@ -1,3 +0,0 @@
-export const SEARCH_URL = "https://youtube-search1.p.rapidapi.com/";
-export const API_KEY = '7b20bbb413msh78726f8eee9007ap132e26jsnaac64d1b9851';
-export const API_HOST = 'youtube-search1.p.rapidapi.com';
